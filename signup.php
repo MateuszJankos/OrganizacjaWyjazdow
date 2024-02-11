68b84499a0720da843b3062b02e3ae9dc2d1cd14
MateuszJankos <jankowskim1002@gmail.com>
2024-02-11 19:15:04 +0100

    dodajemy signup.php

--- signup.php
+++ signup.php
@@ -1,6 +1,18 @@
-<section class="signup-form">
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Logowanie</title>
+  </head>
+  <body>
+  <section class="signup-form">
     <h2>Sign Up</h2>
     <form action="signup.inc.php" method="post">
-    
+        <input type="text" name="name" placeholder="Podaj swoje imie">
+        <input type="text" name="email" placeholder="Podaj Email">
+        <input type="text" name="uid" placeholder="Nazwa użytkownika">
+        <input type="password" name="pwd" placeholder="Hasło">
+        <input type="password" name="pwdrepeat" placeholder="Powtorz hasło">
+        <button type="submit" name="submit">Zarejestruj sie</button>
     </form>
-</section>
+  </body>
+</html>
