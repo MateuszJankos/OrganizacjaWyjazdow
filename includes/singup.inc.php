aabff6e76deb3d01d8ab548ee3f2bdf1fdc3905c
MateuszJankos <jankowskim1002@gmail.com>
2024-02-12 15:58:44 +0100

    więcej funkcji

--- includes/singup.inc.php
+++ includes/singup.inc.php
@@ -1,7 +1,16 @@
 <?php
 
 if (isset($_POST["submit"])) {
-    echo "Dziala poprawnie";
+    
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $username = $_POST["uid"];
+    $pwd = $_POST["pwd"];
+    $pwdRepeat = $_POST["pwdrepeat"];
+
+    require_once 'dbh.inc.php';
+    require_once 'functions.inc.php';
+
 }
 else {
     header("location: ../signup.php");
