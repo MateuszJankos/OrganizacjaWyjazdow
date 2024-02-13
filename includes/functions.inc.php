b32eda1404e367ce2d4195b43839ab032963cb7a
MateuszJankos <jankowskim1002@gmail.com>
2024-02-13 17:47:51 +0100

    Działający system logowania

--- includes/functions.inc.php
+++ includes/functions.inc.php
@@ -84,3 +84,34 @@ function createUser($conn, $name, $email, $username, $pwd) {
     header("location: ../signup.php?error=none");
     exit();
 }
+// sprawdzanie czy pola logowania są puste
+function emptyInputLogin($username, $pwd) {
+    $result;
+    if (empty($username) || empty($pwd)) {
+        $result = true;
+    }
+    else {
+        $result = false;
+    }
+    return $result;
+}
+// funkcja logowania do konta
+function loginUser($conn, $username, $pwd) {
+    $uidExists = uidExists($conn, $username, $username);
+
+    if ($uidExists === false) {
+        header("location: ../login.php?error=wronglogin");
+        exit();
+    }
+
+    $pwdHashed = $uidExists["usersPwd"];
+    $checkPwd = password_verify($pwd, $pwdHashed);
+
+    if ($checkPwd === false) {
+        session_start();
+        $_SESSION["userid"] = $uidExists["usersId"];
+        $_SESSION["useruid"] = $uidExists["usersUid"];
+        header("location: ../hotel.html");
+        exit();
+    }
+}
