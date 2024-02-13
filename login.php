29de0e7def25c93fa218c050ef9b12384809e854
MateuszJankos <jankowskim1002@gmail.com>
2024-02-13 17:27:15 +0100

    Budujemy system logowania

--- login.php
+++ login.php
@@ -7,7 +7,7 @@
   <section class="signup-form">
     <h2>Logowanie</h2>
     <form action="includes/login.inc.php" method="post">
-        <input type="text" name="name" placeholder="Imie/Email">
+        <input type="text" name="name" placeholder="Imie Nazwisko/Email">
         <input type="password" name="pwd" placeholder="Haslo">
         <button type="submit" name="submit">Zaloguj sie</button>
     </form>
