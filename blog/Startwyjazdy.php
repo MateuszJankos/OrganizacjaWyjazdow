2b1ff3470d79b3e324239c0ca672a5b085a9b805
MateuszJankos <jankowskim1002@gmail.com>
2024-02-27 19:08:52 +0100

    Dodano nazwę grupy

--- blog/Startwyjazdy.php
+++ blog/Startwyjazdy.php
@@ -6,9 +6,10 @@ require_once '../includes/dbh.inc.php'; // Połączenie z bazą danych
 if (isset($_SESSION['userid'])) {
     $userId = $_SESSION['userid'];
 
-    // Pobranie wyjazdów powiązanych z użytkownikiem
-    $tripsQuery = "SELECT wyjazd.ID_Wyjazdu, wyjazd.Data_Startu, wyjazd.Data_Konca FROM wyjazd
+    // Pobranie wyjazdów powiązanych z użytkownikiem, wraz z nazwą grupy
+    $tripsQuery = "SELECT wyjazd.ID_Wyjazdu, wyjazd.Data_Startu, wyjazd.Data_Konca, grupa.Nazwa_Grupy FROM wyjazd
                    JOIN uczestnik_grupy ON wyjazd.ID_Grupy = uczestnik_grupy.ID_Grupy
+                   JOIN grupa ON wyjazd.ID_Grupy = grupa.ID_Grupy
                    WHERE uczestnik_grupy.usersId = ?";
     $stmt = $conn->prepare($tripsQuery);
     $stmt->bind_param("i", $userId);
@@ -24,7 +25,7 @@ if (isset($_SESSION['userid'])) {
         <?php foreach ($trips as $trip): ?>
             <div class="card mt-3">
                 <div class="card-body">
-                    <h5 class="card-title">Wyjazd</h5>
+                    <h5 class="card-title">Wyjazd z grupą: <?= htmlspecialchars($trip['Nazwa_Grupy']) ?></h5>
                     <p>Data rozpoczęcia: <?= htmlspecialchars($trip['Data_Startu']) ?></p>
                     <p>Data zakończenia: <?= htmlspecialchars($trip['Data_Konca']) ?></p>
                     <!-- Tutaj możesz dodać inne informacje dotyczące wyjazdu -->
